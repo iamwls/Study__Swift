bc4844f51d6d7981d23be8163ac0678b8cf9e24e
Ye_0nii <62657732+ye-wonii@users.noreply.github.com>
2020-09-05 20:34:18 +0900

    Initial Commit

---
+++ NiceApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NiceApp
+//
+//  Created by 진예원 on 2020/09/05.
+//  Copyright © 2020 진예원. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
